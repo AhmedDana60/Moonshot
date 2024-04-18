4c7f165f4ddb6fe68ab44d58dc5a05ee2422ca19
AhmedDana60 <ahmeddanamo@gmail.com>
2024-04-18 16:25:39 +0300

    MyFirstProjectAddedToGit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ahmed Dana Mohammed on 1/9/23.
+//
+
+import Foundation
+
+
+struct Astronaut : Codable, Identifiable {
+    let id : String
+    let name : String
+    let description : String
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ahmed Dana Mohammed on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
